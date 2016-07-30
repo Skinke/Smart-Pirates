e485191b9728c3b340d10399ae1d6d55caa8ea1b
unknown <henrik_dn@hotmail.com>
2016-07-30 20:55:22 +0200

    no message

---
+++ Smart Pirates 2.0 beta/Assets/Scripts/GameManager.cs
@@ -0,0 +1,170 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+
+public class GameManager : MonoBehaviour
+{
+    //player prefab
+    public GameObject playerPrefab;
+    
+    //Create player list
+    private List<GameObject> playerList;
+
+    private int playerCount;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //Initialise player list
+        playerList = new List<GameObject>();
+
+        // Initialise Airconsole
+        AirConsole.instance.onConnect += OnConnect;
+        AirConsole.instance.onMessage += OnMessage;
+        // AirConsole.instance.onDisconnect += OnDisconnect;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    //When a device connects, spawn a player prefab and assign the tag "CubePlayer + PlayerID" to it
+    //SetActivePlayers(1) and GetControllerDeviceIds >=1, is 1 player. SetActivePlayers(2) and GetControllerDeviceIds >=2 = 2 player
+    void OnConnect(int device_id)
+    {
+
+        if (AirConsole.instance.GetActivePlayerDeviceIds.Count == 0)
+        {
+            if (AirConsole.instance.GetControllerDeviceIds().Count >= 1)
+            {
+                AirConsole.instance.SetActivePlayers(1);
+                
+
+            }
+            else
+            {
+                AirConsole.instance.SetActivePlayers(0);
+            }
+        }
+
+
+        int active_player = AirConsole.instance.ConvertDeviceIdToPlayerNumber(device_id);
+
+        if (active_player != -1)
+        {
+          
+
+            AddPlayer();
+
+            //int playerID = active_player;
+            // GameObject cubePlayer = Instantiate(myCube, new Vector3(0, 2, 0), Quaternion.identity) as GameObject;
+            //cubePlayer.gameObject.tag = "CubePlayer" + playerID;
+            //cubePlayer.name = "CubePlayer" + playerID;
+
+        }
+
+
+    }
+
+
+
+    public void AddPlayer()
+    {
+        //Instantiate new player Object
+        GameObject tmp = (GameObject)Instantiate(playerPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        //Rename player object not strictly necessary
+        tmp.name = "player" + (playerCount).ToString();
+        playerCount++;
+
+
+        //Add to list
+        playerList.Add(tmp);
+    }
+
+
+
+
+    void OnMessage(int device_id, JToken data)
+    {
+
+
+        print(data);
+        //print((float)data["x"]);
+       
+
+
+        int active_player = AirConsole.instance.ConvertDeviceIdToPlayerNumber(device_id);
+                if (active_player != -1)
+                {
+                    if (active_player == 0)
+                    {
+                playerList[0].GetComponent<MovePlayer>().MoveShip((float)data["x"]);
+               
+                //latency 
+                long latency_ms = AirConsole.instance.GetServerTime() - (long)data["ts"];
+                     Debug.Log("device " + " latency: " + latency_ms);
+                
+
+
+                playerList[0].GetComponent<MovePlayer>().MoveIt((float)data["move"]);
+                      print(playerList[0].name + " is moving");
+                
+                
+
+                
+            }
+                    if (active_player == 1)
+                    {
+                        playerList[1].GetComponent<MovePlayer>().MoveIt((float)data["move"]);
+                        print(playerList[1].name + " is moving");
+
+                
+
+                
+            }
+                }
+
+      
+    }
+
+
+
+    /*
+        void OnDisconnect(int device_id)
+        {
+            int active_player = AirConsole.instance.ConvertDeviceIdToPlayerNumber(device_id);
+
+            if (active_player != -1)
+            {
+                if (AirConsole.instance.GetControllerDeviceIds().Count >= 2)
+                {
+                   AirConsole.instance.SetActivePlayers(2);
+                }
+                else
+                {
+                   AirConsole.instance.SetActivePlayers(0);
+
+                }
+            }
+        }
+    */
+
+
+
+
+
+}
+
+
+  
+
---
+++ Smart Pirates 2.0 beta/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class MovePlayer : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    public float speed = 5f;
+    //public float rotateSpeed = 100f;
+    Vector3 _dir = Vector3.zero;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+        controller = GetComponent<CharacterController>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 movement = _dir * speed;
+        movement *= Time.deltaTime;
+
+        controller.Move(movement);
+
+        //transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(_dir), rotateSpeed * Time.smoothDeltaTime);
+    }
+
+
+
+    public void MoveIt(float amount)
+    {
+
+        //transform.position += new Vector3(0f, 0f, 1f);
+        transform.Translate(new Vector3(0f, 0f, amount));
+    }
+
+    public void MoveShip(float x)
+    {
+        print("hej");
+        
+
+
+        Vector3 dir = new Vector3(x, 0f, 0);
+   /*     if (dir.sqrMagnitude > 1f)
+        {
+            dir.Normalize();
+        }
+
+        if (dir.sqrMagnitude < 0.03f)
+        {
+            return;
+        }
+*/
+        _dir = dir;
+        print(dir);
+    }
+
+
+
+
+
+}
